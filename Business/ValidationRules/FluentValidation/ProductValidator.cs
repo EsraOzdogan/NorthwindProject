f66c7f624dc21587b19e27fe781905b903cc14ae
EsraOzdogan <esraozdogaan@gmail.com>
2021-04-15 03:02:07 +0300

    fluent validation

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Business.ValidationRules
+namespace Business.ValidationRules.FluentValidation
 {
     public class ProductValidator: AbstractValidator<Product>
     {
